8997360b0b5d62b9369b6311e92f926ceb3207af
halzathalik <halzathalik@gmail.com>
2023-08-13 18:56:34 -0400

    if statement final update

--- practice/js/IfStatement.js
+++ practice/js/IfStatement.js
@@ -6,7 +6,7 @@ if (true){
 
 //practical example
 
-let age = Math.floor(Math.random()*3);
+let age = Math.floor(Math.random()*30);
 console.log(`Your age is `+age);
 if (age >= 16){
     console.log('Allowed to drive');
@@ -17,4 +17,24 @@ if (age >= 16){
  else {
     console.log("Not allowed to drive");
 }
+
+
+ /*
+    falsy values:
+        false   0   ''  Nan     undefined    null
+  */
+
+if (0){
+    console.log('nope');
+}else {
+    console.log('yeah')
+}
+
+
+//ternary operator
+console.log(age>16?'you can drive':'you canNOT drive');
+
+const msg = 5 && 'hello';
+console.log(msg);
+
 
