baff80d01d650db79e99e09dc53ed2efc4ef542d
halzathalik <halzathalik@gmail.com>
2023-08-13 22:26:36 -0400

    rock-scissors-paper game updated

---
+++ practice/js/Functions.js
@@ -0,0 +1,13 @@
+function sayHello(){
+    console.log('hello');
+}
+
+ function sayHello2(name){
+    console.log(`Hello ${name}`);
+}
+
+function isEvenNum(num){
+    return num%2===0;
+}
+sayHello2('Halzat');
+console.log(evenNum(4));
