8ef0edb8416939efc57b83484e2ae6ede44086c6
halzathalik <halzathalik@gmail.com>
2023-08-16 22:48:26 -0400

    more objects

--- practice/js/Objects.js
+++ practice/js/Objects.js
@@ -36,3 +36,44 @@ product2.fun();
 console.log(JSON.stringify(product2));
 console.log('======================');
 console.log(JSON.parse(JSON.stringify(product2)));
+console.log("=============Auto Boxing===============");
+console.log('hello'.length);
+console.log('hello'.toUpperCase());
+
+const obj1 = {
+    msg: 'hello'
+};
+const obj2 = obj1;// obj2 makes the copy of obj's reference
+
+obj1.msg = 'Good job!';
+console.log(obj1);
+
+const obj3 = {
+    msg: 'Good job!'
+};
+console.log(obj3 === obj1); //Nope
+console.log(obj2 === obj1);
+
+//destructuring
+const obj4 = {
+    msg: 'Good job!',
+    price: 799
+};
+//const msg = obj4.msg;
+const {msg,price} = obj4;
+console.log(msg);
+console.log(price);
+
+//shorthand property
+const obj5 = {
+    //msg: msg
+    msg,
+    // method: function function1(){
+    //     console.log('method')
+    // }
+    method(){
+        console.log('method')
+    }
+}
+console.log(obj5);
+obj5.method();
